a4fdc7acd811e231703a6b9d588bb57968176894
dynamiclc <dynamiclc@gmail.com>
2014-01-27 18:06:17 +0900

    Update MergeSortLinkedList.java
    Ripped out the main() and put it into Main.java; renamed the package.

--- MergeSortLinkedList.java
+++ MergeSortLinkedList.java
@@ -1,7 +1,7 @@
-package algorithm.mergesort;
+package problem.mergesort;
 
-import datastructure.listnode.ListNode;
- 
+import datastructure.listnode.*;
+
 public class MergeSortLinkedList {
   // Merge sort
   public static ListNode mergeSortList(ListNode head) {
@@ -94,25 +94,5 @@ public class MergeSortLinkedList {
       }
       System.out.println();
     }
-  }
-  
-  public static void main(String[] args) {
-    // Generate a sample input [2, 3, 4, 3, 4]
-    ListNode n1 = new ListNode(2);
-    ListNode n2 = new ListNode(3);
-    ListNode n3 = new ListNode(4);
-    ListNode n4 = new ListNode(3);
-    ListNode n5 = new ListNode(4);
- 
-    n1.next = n2;
-    n2.next = n3;
-    n3.next = n4;
-    n4.next = n5;
-        
-    // Merge sort the sample input
-    n1 = mergeSortList(n1);
-    
-    // Print out result
-    printList(n1);
   }
 }
