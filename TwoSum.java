4c6e7e313bfa902d6678d750cab54ac5b70f3e91
dynamiclc <dynamiclc@gmail.com>
2014-01-28 16:09:10 +0900

    Update TwoSum.java
    Accepted answer at http://oj.leetcode.com/problems/two-sum/

--- TwoSum.java
+++ TwoSum.java
@@ -4,51 +4,37 @@ import java.util.*;
 
 public class TwoSum {
   public static int[] twoSum(int[] numbers, int target) {
-    Map<Integer, Integer> m = new HashMap<Integer, Integer>();
-    int n = numbers.length; // count of all numbers 
+    // 1. Initialization
+    
+    // 1.1 The result
     int[] result = new int[2];
     
-    for (int i = 0; i < n; i++) {
-      if (m.isEmpty()) { // if m is empty
-        m.put(numbers[i], i);
-      } else { // if m is not empty        
-        Integer key = new Integer(target - numbers[i]); // key is the subscript of the number that is complement to the 
-        Integer key2 = new Integer(numbers[i]); // key2 is the subscript of number currently
-        if (m.containsKey(key)){
-          result[0] = m.get(key).intValue() + 1;
-          System.out.println("index1=" + result[0]);          
-          result[1] = i + 1;
-          System.out.println("index2=" + result[1]);
-          break;
-        } else if (!m.containsKey(key2)) {
-          m.put(key2,i);
-        } else{          
-        }
-      }
-    }
-    return result;
-  }
-}
+    // 1.2 The count of numbers
+    int n = numbers.length;
+    
+    // 1.3 The HashMap
+    Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+    
+    // 2. Calculation
 
-//public int[] twoSum(int[] numbers, int target) {  
-//    // Start typing your Java solution below  
-//    // DO NOT write main() function  
-//    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();  
-//    int n = numbers.length;  
-//    int[] result = new int[2];  
-//    for (int i = 0; i < numbers.length; i++)  
-//    {  
-//        if (map.containsKey(target - numbers[i]))  
-//        {  
-//            result[0] = map.get(target-numbers[i]) + 1;  
-//            result[1] = i + 1;  
-//            break;  
-//        }  
-//        else  
-//        {  
-//            map.put(numbers[i], i);  
-//        }  
-//    }  
-//    return result;  
-//      
-//}  
+    // 2.1 Complement of the target, number value and index
+    int numberComplement = 0;
+    int indexComplement = 0;
+
+    // 2.2 Loop through each element trying to find its complement
+    for (int i = 0; i < n; i++) {
+      numberComplement = target - numbers[i];
+      if (hashMap.containsKey(numberComplement)) {
+        indexComplement = hashMap.get(numberComplement);
+        result[0] = indexComplement + 1;
+        result[1] = i + 1;
+        break;
+      } else {
+        hashMap.put(numbers[i], i);
+      } 
+    }
+    
+    // 3. Return
+    return result;
+  } // End of method twoSum()
+} // End of class SolutionTwoSum
